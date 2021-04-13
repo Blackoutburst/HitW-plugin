3594c6de607d8b64f948aadc0d7f874c00c0c57a
Blackoutburst <blackoutburst@hotmail.com>
2021-04-13 17:33:37 +0200

    Changed ranks color to match new discord color

--- src/utils/RankManager.java
+++ src/utils/RankManager.java
@@ -32,7 +32,7 @@ public class RankManager {
 			}
 		}
 		if (player.getUniqueId().toString().replace("-", "").toString().equals("9293868b414c42b2bd8e3bcb791247b9")) {
-			rank = ChatColor.DARK_RED+"["+ChatColor.GRAY+"Yaku"+ChatColor.DARK_RED+"]"+ChatColor.WHITE;
+			rank = ChatColor.DARK_RED+"["+ChatColor.DARK_GRAY+"Yaku"+ChatColor.DARK_RED+"]"+ChatColor.DARK_BLUE;
 		} else {
 			rank = getRank(player, qualification, finals);
 		}
@@ -50,38 +50,65 @@ public class RankManager {
 	 * @author Blackoutburst
 	 */
 	private static String getRank(Player player, int qualification, int finals) {
-		if (qualification >= 400 || finals >= 400) {
+		if (qualification >= 500 || finals >= 500) {
+			return(ChatColor.DARK_GREEN + "[500+] ");
+			
+		} else if (qualification >= 475 || finals >= 475) {
+			return(ChatColor.GREEN + "[450"+ ChatColor.DARK_GREEN +"+"+ChatColor.GREEN+"] ");
+			
+		} else if (qualification >= 450 || finals >= 450) {
+			return(ChatColor.GREEN + "[450+] ");
+			
+		} else if (qualification >= 425 || finals >= 425) {
+			return(ChatColor.DARK_AQUA + "[400"+ ChatColor.GREEN +"+"+ChatColor.DARK_AQUA+"] ");
+			
+		} else if (qualification >= 400 || finals >= 400) {
 			return(ChatColor.DARK_AQUA + "[400+] ");
+			
 		} else if (qualification >= 375 || finals >= 375) {
-			return(ChatColor.YELLOW + "[350"+ ChatColor.DARK_AQUA +"+"+ChatColor.YELLOW+"] ");
+			return(ChatColor.BLUE + "[350"+ ChatColor.DARK_AQUA +"+"+ChatColor.BLUE+"] ");
+			
 		} else if (qualification >= 350 || finals >= 350) {
-			return(ChatColor.YELLOW + "[350+] ");
+			return(ChatColor.BLUE + "[350+] ");
+			
 		} else if (qualification >= 325 || finals >= 325) {
-			return(ChatColor.GOLD + "[300"+ ChatColor.YELLOW +"+"+ChatColor.GOLD+"] ");
+			return(ChatColor.DARK_PURPLE + "[300"+ ChatColor.BLUE +"+"+ChatColor.DARK_PURPLE+"] ");
+			
 		} else if (qualification >= 300 || finals >= 300) {
-			return(ChatColor.GOLD + "[300+] ");
+			return(ChatColor.DARK_PURPLE + "[300+] ");
+			
 		} else if (qualification >= 275 || finals >= 275) {
-			return(ChatColor.LIGHT_PURPLE + "[250"+ ChatColor.GOLD +"+"+ChatColor.LIGHT_PURPLE+"] ");
+			return(ChatColor.LIGHT_PURPLE + "[250"+ ChatColor.DARK_PURPLE +"+"+ChatColor.LIGHT_PURPLE+"] ");
+			
 		} else if (qualification >= 250 || finals >= 250) {
 			return(ChatColor.LIGHT_PURPLE + "[250+] ");
+			
 		} else if (qualification >= 225 || finals >= 225) {
-			return(ChatColor.DARK_PURPLE + "[200"+ ChatColor.LIGHT_PURPLE +"+"+ChatColor.DARK_PURPLE+"] ");
+			return(ChatColor.DARK_RED + "[200"+ ChatColor.LIGHT_PURPLE +"+"+ChatColor.DARK_RED+"] ");
+			
 		} else if (qualification >= 200 || finals >= 200) {
-			return(ChatColor.DARK_PURPLE + "[200+] ");
+			return(ChatColor.DARK_RED + "[200+] ");
+			
 		} else if (qualification >= 175 || finals >= 175) {
-			return(ChatColor.RED + "[150"+ ChatColor.DARK_PURPLE +"+"+ChatColor.RED+"] ");
+			return(ChatColor.RED + "[150"+ ChatColor.DARK_RED +"+"+ChatColor.RED+"] ");
+			
 		} else if (qualification >= 150 || finals >= 150) {
 			return(ChatColor.RED + "[150+] ");
+			
 		} else if (qualification >= 125 || finals >= 125) {
-			return(ChatColor.DARK_RED + "[100"+ ChatColor.RED +"+"+ChatColor.DARK_RED+"] ");
+			return(ChatColor.GOLD + "[100"+ ChatColor.RED +"+"+ChatColor.GOLD+"] ");
+			
 		} else if (qualification >= 100 || finals >= 100) {
-			return(ChatColor.DARK_RED + "[100+] ");
+			return(ChatColor.GOLD + "[100+] ");
+			
 		} else if (qualification >= 75 || finals >= 75) {
-			return(ChatColor.GREEN + "[50"+ ChatColor.DARK_RED +"+"+ChatColor.GREEN+"] ");
+			return(ChatColor.YELLOW + "[50"+ ChatColor.GOLD +"+"+ChatColor.YELLOW+"] ");
+			
 		} else if (qualification >= 50 || finals >= 50) {
-			return(ChatColor.GREEN + "[50+] ");
+			return(ChatColor.YELLOW + "[50+] ");
+			
 		} else {
-			return(ChatColor.DARK_GREEN+"");
+			return(ChatColor.GRAY+"");
 		}
 	}
 	
