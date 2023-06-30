3b76afb1f4ff5bcf27525ca327cc653e8685781b
FubyCutie <roset280905@gmail.com>
2023-06-30 01:30:25 +0100

    fixed null pointer with useage of NoteBlockAPI

--- src/main/java/event/Join.java
+++ src/main/java/event/Join.java
@@ -1,27 +1,20 @@
 package event;
 
-import java.io.File;
-
 import analytics.AnalyticsActions;
 import analytics.AnalyticsWatcher;
 import com.xxmicloxx.NoteBlockAPI.NoteBlockAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import com.blackout.npcapi.core.PacketInteractListener;
 import com.blackoutburst.simplenpc.NPCFile;
 import com.blackoutburst.simplenpc.SimpleNPCPlayer;
 
-import core.Board;
 import core.HPlayer;
 import main.Main;
 import utils.NPCListener;
 import utils.NPCUtils;
-import utils.RankManager;
-import utils.ScoreboardManager;
 import utils.Utils;
 
 public class Join {
@@ -37,10 +30,7 @@ public class Join {
 		event.getPlayer().teleport(new Location(Bukkit.getWorld("world"), -7.5f, 55, -1054.5f, 0, 0));
 		Utils.giveConfigItem(event.getPlayer());
 		event.getPlayer().setAllowFlight(p.isFly());
-		NoteBlockAPI.getSongPlayersByPlayer(event.getPlayer()).forEach(rsp -> {
-			rsp.setPlaying(false);
-			rsp.destroy();
-		});
+		NoteBlockAPI.stopPlaying(event.getPlayer());
 
 		PacketInteractListener.init(event.getPlayer(), new NPCListener());
 		NPCUtils.spawnNPC(event);
